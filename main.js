330d0c59bcdb330a0d32d8f0cd7aacda7a054384
anastasijasv <sviridova.anastasija@gmail.com>
2022-05-14 12:42:31 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// Temperature in Kelvin
+const kelvin = 293;
+
+// Temperature in Celsius
+const celsius = kelvin - 273;
+
+// Temperature in Fahrenheit
+let fahrenheit = celsius * (9 / 5) + 32;
+
+// Rounded down temperature in Fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
