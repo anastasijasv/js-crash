952e1854f1d451c5f85371de48c83433bc76ec0e
anastasijasv <sviridova.anastasija@gmail.com>
2022-05-20 13:16:50 +0300

    js crash course

--- main.js
+++ main.js
@@ -1,16 +1,58 @@
-// Temperature in Kelvin
-const kelvin = 293;
+console.log('Hello World');
 
-// Temperature in Celsius
-const celsius = kelvin - 273;
+const name = 'John';
+const age = 35;
 
-// Temperature in Fahrenheit
-let fahrenheit = celsius * (9 / 5) + 32;
+// Concatenation
+console.log('My name is ' + name + ' and I am ' + age);
+// Template String
+console.log(`My name is ${name} and I am ${age}`);
 
-// Rounded down temperature in Fahrenheit
-fahrenheit = Math.floor(fahrenheit);
+const string = 'Hello World!';
+console.log(string.toUpperCase());
 
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+const s = 'technology, computers, it, code';
+console.log(s.split(', '));
+
+// Arrays - variables that hold multiple values
+const numbers = new Array(1,2,3,4,5);
+console.log(numbers);
+
+const fruits = ['apples', 'oranges', 'pears'];
+console.log(fruits[1]);
+fruits[3] = 'grapes';
+fruits.push('mangos');
+fruits.unshift('strawberries');
+console.log(fruits);
+
+console.log(Array.isArray(fruits));
+console.log(fruits.indexOf('oranges'));
+
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 35,
+    hobbies: ['music', 'movies', 'sports'],
+    address: {
+        street: '50 Main St.',
+        city: 'Boston',
+        state: 'MA'
+    }
+}
+
+console.log(person);
+console.log(person.firstName, person.lastName);
+console.log(person.hobbies[1]);
+console.log(person.address.city);
+
+const { firstName, lastName, address: { city }} = person;
+console.log(city);
+
+person.email = 'john@gmail.com';
+console.log(person);
+
+
+
 
 
 
